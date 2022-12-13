ff83b69928c4b2718f562beecefc2e041fc9a245
mwangashi <mwangashiw@gmail.com>
2022-12-12 22:22:12 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * main - Outputs exact words using printf
+ * Return: will retutn 0 uless there is an error
+int main(void)
+{
+
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
