0c6401a4ac9d70d24d1d71daac5d2671a03d89d8
mwangashi <mwangashiw@gmail.com>
2022-12-13 11:51:11 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - prints the alphabet in lowercase,
- *   * followed by a new line, except q and e
- *    * Return: Always 0 (Sucess)
- *     */
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 char ch = 'a';
